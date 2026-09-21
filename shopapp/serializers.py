f95c53946ab366fdbb0cd04dee00a82093a52669
anseefkkv <308718423+Anseefv@users.noreply.github.com>
2026-09-21 15:18:59 +0530

    serializer created

---
+++ shopapp/serializers.py
@@ -0,0 +1,28 @@
+from rest_framework  import serializers
+from .models import User
+from django.contrib.auth.hashers import make_password
+
+class RegisterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=User
+        fields=['username','email','password']
+
+
+    def validate_password(self,value):
+        if len(value)<8:
+            raise serializers.ValidationError("password must be 8 characters")
+        return value
+
+    def create(self, validated_data):
+
+        data={
+        "username":validated_data.get('username'),
+        "email":validated_data.get('email'),
+        "password":make_password(validated_data.get('password'))
+        }
+
+        user=User.objects.create(**data)
+        
+        return user
+        
+
