b2390ae29dbbf6de10f46306b51e366c4a847e45
anseefkkv <308718423+Anseefv@users.noreply.github.com>
2026-09-21 17:52:52 +0530

    access code returned

--- shopapp/serializers.py
+++ shopapp/serializers.py
@@ -1,11 +1,14 @@
 from rest_framework  import serializers
 from .models import User
-from django.contrib.auth.hashers import make_password
+from django.contrib.auth.hashers import make_password,check_password
+
+from rest_framework_simplejwt.tokens import AccessToken
+
 
 class RegisterSerializer(serializers.ModelSerializer):
 
     password = serializers.CharField(write_only=True)
-    
+
     class Meta:
         model=User
         fields=['username','email','password']
@@ -27,5 +30,35 @@ class RegisterSerializer(serializers.ModelSerializer):
         user=User.objects.create(**data)
         
         return user
+
+
+class LoginSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+    email=serializers.EmailField()
+    class Meta:
+        model=User
+        fields=['email','password']
         
+    
+    def validate(self, attrs):
+        email=attrs.get('email')
+        user = User.objects.filter(email=email).first()
+        if not user:
+            raise serializers.ValidationError("Invalid email or password")
+        password=attrs.get('password')
+        if not check_password(password,user.password):
+            raise serializers.ValidationError("Invalid email or password")
+
+        attrs['user'] = user
+
+        return attrs
+
+    
+
+    def create(self, validated_data):
+        token=AccessToken()
+        token['user_id']=validated_data.get('user').id
+        token['role']=validated_data.get('user').role
+
+        return str(token)
 
--- shopapp/views.py
+++ shopapp/views.py
@@ -1,17 +1,30 @@
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.generics import CreateAPIView
+from rest_framework import status
 
 from .models import User
-from .serializers import RegisterSerializer
+from .serializers import RegisterSerializer,LoginSerializer
 
 
 
 class RegisterView(CreateAPIView):
     serializer_class=RegisterSerializer
 
+
+class LoginView(CreateAPIView):
+
+    def post(self, request):
+        ser=LoginSerializer(data=request.data)
+        if ser.is_valid():
+            token=ser.save()
+            return Response({"access":token})
+        return Response (ser.errors,status=status.HTTP_400_BAD_REQUEST)
+
+
+
     
 
 
 
-# Create your views here.
+
